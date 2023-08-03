3c386e22af6b4b98b8caa9e866e474a744bbc6a4
lake-crimsonn <lakecrimsonn@gmail.com>
2023-08-03 22:52:10 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch("/users")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>user memebers</h1>
+      {typeof data.members === "undefined" ? (
+        <h3>loading..</h3>
+      ) : (
+        data.members.map((u) => <p key={u.id}>{u.name}</p>)
+      )}
     </div>
   );
 }
